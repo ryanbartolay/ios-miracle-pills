90a2b2852a36068813add201d4dc61f09c845a2b
Ryan <bartolay.ryan@gmail.com>
2017-08-31 19:12:47 +0800

    initial files

---
+++ Miracle-pills/Miracle-pills/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Miracle-pills
+//
+//  Created by gotech on 31/08/2017.
+//  Copyright © 2017 gotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.purple
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
