444203071be7665d1bd51e63faed1b70c08478ba
Ryan <bartolay.ryan@gmail.com>
2017-09-04 19:26:53 +0800

    placed a picker

--- Miracle-pills/Miracle-pills/ViewController.swift
+++ Miracle-pills/Miracle-pills/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    let states = ["Manila", "Makati", "Las Pinas", "Alabang"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        statePicker.dataSource = self
+        statePicker.delegate = self
         //self.view.backgroundColor = UIColor.purple
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -21,7 +26,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var stateButtonPressed: UIButton!
+    
+    @IBAction func stateButtonPressed(_ sender: Any) {
+    }
+    // number of columns
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
 }
 
