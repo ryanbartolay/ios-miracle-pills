ff778475c8c16d50144283cd8e16efd63b24e85a
Ryan <bartolay.ryan@gmail.com>
2017-09-05 20:39:01 +0800

    Swapping of pages initial

--- ScrollViews/PageWithScroll/PageWithScroll/ViewController.swift
+++ ScrollViews/PageWithScroll/PageWithScroll/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for x in 0...1 {
+            let image = UIImage(named: "icon\(x).png")
+            images.append(UIImageView(image: image))
+        }
+        
+        print("Count \(images.count)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ SwappingScreens/SwappingScreens/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by gotech on 05/09/2017.
+//  Copyright © 2017 gotech. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
